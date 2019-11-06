7d38ed6d5f4e80f6395569323875df44bb940aba
zlx-lixia <57438098+zlx-lixia@users.noreply.github.com>
2019-11-06 21:15:10 +0800

    Update AdminTest.java

--- src/test/java/com/baizhi/test/AdminTest.java
+++ src/test/java/com/baizhi/test/AdminTest.java
@@ -10,6 +10,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class AdminTest {
 	@Test
 	public void Test1(){
+		
+		//在页面的修改，在本地查看
 		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
 		AdminDao adminDao=(AdminDao) ac.getBean("adminDao");
 		adminDao.addAdmin(new Admin("1","lmm","李喵喵","123123","男"));
